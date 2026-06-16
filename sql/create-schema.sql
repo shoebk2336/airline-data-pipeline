ef0546429d20d460c93764a866077a5ce27bdd1d
SHOEB KHAN <113032605+shoebk2336@users.noreply.github.com>
2026-06-17 00:51:28 +0530

    Create airlines schema and tables with user permissions
    Created schema and tables for airline data management, including airport and daily flight facts. Added user permissions for glue_user.

--- sql/create-schema.sql
+++ sql/create-schema.sql
@@ -1,1 +1,28 @@
+create SCHEMA airlines;
 
+
+create TABLE airlines.airport_dim(
+    airport_id BIGINT,
+    city VARCHAR,
+    state VARCHAR,
+    name VARCHAR
+)
+
+
+create TABLE airlines.daily_flight_facts(
+    carrier VARCHAR,
+    dep_airport VARCHAR,
+    arr_airport VARCHAR,
+    dep_city VARCHAR,
+    arr_city VARCHAR,
+    dep_state VARCHAR,
+    arr_state VARCHAR,
+    dep_delay BIGINT,
+    arr_delay BIGINT
+)
+
+#To create user and password
+
+CREATE USER glue_user PASSWORD '[Smkhan@1997]!';
+GRANT ALL ON SCHEMA airlines TO glue_user;
+GRANT ALL ON ALL TABLES IN SCHEMA airlines TO glue_user;
